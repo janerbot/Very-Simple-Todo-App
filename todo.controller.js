8b27e298931a6944d3fa8e4224def5929a0abf71
janerbot <janerbot@users.noreply.github.com>
2017-03-13 17:18:29 -0700

    Update todo.controller.js

--- todo.controller.js
+++ todo.controller.js
@@ -2,39 +2,34 @@
 angular
 .module('todoListApp',[]);
 .controller('todoController', todoController);
-
 function todoController() {
-var vm = this;
-	vm.order = '3'
-	vm.hide = true;
-	vm.todoList = [];
-	vm.tasks = [
-		{'Task 1','High'}
-		{'Task 2','Low'}
-		{'Task 3','Medium'}
-	];
-
-	vm.count = 0
-	vm.addTask = function () {
-		if (vm.tasks){
-				vm.todoList.push([vm.task, vm.count]);
-				vm.task = '';
-				vm.priority = '';
-				vm.hide = false;
-				vm.count++;
-			}
-		}
-				vm.high = function high() {
-		        vm.order = "1";
-		    }
-		    vm.low = function low() {
-		        vm.order = "-1";
-		    }
-		    vm.medium = function medium() {
-		        vm.order = "0";
-
-		    }
-		}
-
-
+var vm = this;	
+vm.order = '3'	
+vm.hide = true;	
+vm.todoList = [];	
+vm.tasks = [		
+{'Task 1','High'}		
+{'Task 2','Low'}		
+{'Task 3','Medium'}	
+];	
+vm.count = 0	
+vm.addTask = function () {		
+if (vm.tasks){				
+vm.todoList.push([vm.task, vm.count]);				
+vm.task = '';				
+vm.priority = '';				
+vm.hide = false;				
+vm.count++;			
+}		
+}				
+vm.high = function high() {		
+        vm.order = "1";		
+    }		
+    vm.low = function low() {		
+        vm.order = "-1";		
+    }		
+    vm.medium = function medium() {		
+        vm.order = "0";		
+    }		
+}
 });
